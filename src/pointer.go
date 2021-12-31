bf7fbcdbb14bc3a10ff22eb2beca106aa1f394c2
WangPengLiang <15101587969@163.com>
2021-12-31 14:12:33 +0800

    fmt/flag

---
+++ src/pointer.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 指针的&和*
+	x := 1          // 声明int类型变量x
+	p := &x         // &x用于获取变量x的内存地址，返回一个指向x的指针p
+	fmt.Println(*p) // *p用户获取指针p指向变量的值
+	*p = 2          // 可以重新给*p指针赋值
+	fmt.Println(x)  // "2"
+
+	// 引用类型不仅要声明，而且需要分配内存空间
+	// var a *int
+	// *a = 100
+	// fmt.Println(*a)
+
+	// var b map[string]int
+	// b["沙河娜扎"] = 100
+	// fmt.Println(b)
+
+	// new函数得到的是一个类型的指针，并且该指针对应的值为该类型的零值
+	a := new(int)
+	b := new(bool)
+	fmt.Printf("%T\n", a) // *int
+	fmt.Printf("%T\n", b) // *bool
+	fmt.Println(*a)       // 0
+	fmt.Println(*b)       // false
+
+	// make函数只用于 slice、map以及 channel 的内存创建，而且它返回的类型就是这三个类型本身，而不是他们的指针类型
+	var c map[string]int
+	c = make(map[string]int, 10)
+	c["山西一枝花"] = 100
+	fmt.Println(c)
+}
---
+++ src/time.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timeTest()
+}
+
+func timeTest() {
+	// func Now() Time
+	fmt.Println(time.Now())
+
+	// func Parse(layout, value string) (Time, error)
+	time1 := time.Parse("2016-01-02 15:04:05", "2018-04-23 12:24:51")
+	fmt.Println(time1)
+
+	// func ParseInLocation(layout, value string, loc *Location) (Time, error) (layout已带时区时可直接用Parse)
+	time.ParseInLocation("2006-01-02 15:04:05", "2017-05-11 14:06:06", time.Local)
+
+	// func Unix(sec int64, nsec int64) Time
+	time.Unix(1e9, 0)
+
+	// func Date(year int, month Month, day, hour, min, sec, nsec int, loc *Location) Time
+	time.Date(2018, 1, 2, 15, 30, 10, 0, time.Local)
+
+	// func (t Time) In(loc *Location) Time 当前时间对应指定时区的时间
+	loc, _ := time.LoadLocation("America/Los_Angeles")
+	fmt.Println(time.Now().In(loc))
+
+	// func (t Time) Local() Time
+	fmt.Println(time.Local)
+}
+
+func timeBefore() {
+	now := time.Now()
+	fmt.Println(now.Before(now.Add(time.Hour)))
+}
+
+// 时间比较,equal会比较地点和时区信息
+func timeEqual() {
+	now := time.Now()
+	fmt.Println(now.Equal(now.Add(time.Hour)))
+}
+
+// 求两个时间之差
+func timeSub() {
+	now := time.Now()
+	value := now.Sub(now.Add(time.Hour))
+	fmt.Println(value)
+}
+
+// 获取当前时间+指定时间后的时间
+func timeAdd() {
+	now := time.Now()
+	value := now.Add(1) // 当前时间加1小时后的时间
+	fmt.Println(value)
+}
+
+// 将时间戳转换为时间格式
+func unixConverTime() {
+	// 将时间戳转换为时间格式
+	timestamp := time.Now().Unix() //时间戳
+	fmt.Printf("current timestamp1:%v\n", timestamp)
+
+	timeObj := time.Unix(timestamp, 0) //将时间戳转为时间格式
+	fmt.Println(timeObj)
+	year := timeObj.Year()     //年
+	month := timeObj.Month()   //月
+	day := timeObj.Day()       //日
+	hour := timeObj.Hour()     //小时
+	minute := timeObj.Minute() //分钟
+	second := timeObj.Second() //秒
+	fmt.Printf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+}
+
+// 获取时间戳
+func getUnix() {
+	// 获取时间戳
+	timestamp1 := time.Now().Unix()     //时间戳
+	timestamp2 := time.Now().UnixNano() //纳秒时间戳
+	fmt.Printf("current timestamp1:%v\n", timestamp1)
+	fmt.Printf("current timestamp2:%v\n", timestamp2)
+}
+
+// 获取当前时间
+func getTimeNow() {
+	now := time.Now() //获取当前时间
+	fmt.Printf("current time:%v\n", now)
+
+	year := now.Year()     //年
+	month := now.Month()   //月
+	day := now.Day()       //日
+	hour := now.Hour()     //小时
+	minute := now.Minute() //分钟
+	second := now.Second() //秒
+	fmt.Printf("%d-%02d-%02d %02d:%02d:%02d\n", year, month, day, hour, minute, second)
+}
