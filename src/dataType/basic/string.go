bc12688a7ce23f97a5b6466ad8b3ef0b254d1569
WangPengLiang <15101587969@163.com>
2022-03-24 13:56:51 +0800

    update array.go

--- src/dataType/basic/string.go
+++ src/dataType/basic/string.go
@@ -7,38 +7,57 @@ import (
 )
 
 func main() {
-	stringTest()
+	updateStringByteTest()
 }
 
-// 字符串
-func stringTest() {
-	// 字符串字面量语法：可以用来定义一个多行字符串
-	var s1 string = "hello"
-	var s2 string = `hello
-				     world`
-	fmt.Println(s1, s2)
-
-	// 内置函数len：返回字符串的字节数
-	s := "abc北京"
-	fmt.Printf("字节长度：%d \n", len(s)) // output:9
+// 内置函数len：返回字符串的字节数
+func getByteCounTest() {
+	str := "abc北京"
+	fmt.Printf("字节长度：%d \n", len(str)) // output:9
 
 	// 返回字符串每个字节的值
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
+	for i := 0; i < len(str); i++ {
+		fmt.Println(str[i])
 	}
-	// 获取字符串中字符的个数
-	r := []rune(s)
-	fmt.Println(len(r))
+}
 
-	// 字符串截取
+// 获取字符个数
+func getCharCountTest() {
+	str := "abc北京"
+	// 获取字符串中字符的个数
+	r := []rune(str)
+	fmt.Println(len(r))
+}
+
+// 字符串字面量语法：可以用来定义一个多行字符串
+func stringLiteralTest() {
+	var s1 string = "hello"
+	var s2 string = `hello
+				 world`
+	fmt.Println(s1, s2)
+}
+
+// 一个 string 类型的值既可以被拆分为一个包含多个字符的序列，也可以被拆分为一个包含多个字节的序列。
+func stringRunByteTest() {
+	str := "helloworld 世界你好"
+	fmt.Printf("string:%q\n", str)              // 原文格式输出
+	fmt.Printf("rune(char):%q\n", []rune(str))  // 输出[]rune切片
+	fmt.Printf("rune(hex):%x\n", []rune(str))   // 采用16进制数表示
+	fmt.Printf("bytes(hex):% x\n", []byte(str)) // 输出[]byte切片
+}
+
+// 字符串截取
+func subStringTest() {
 	str := "abcdefg"
 	t1 := str[1:4] // startIndex=1,endIndex=4
 	t2 := str[:4]  // 省略第一个索引从0开始，startIndex=0,endIndex=4
 	t3 := str[1:]  // 省略第二个索引，从1开始截取到末尾
 	// t4 := str[:10] //runtime error: slice bounds out of range [:10] with length 7 数组越界
 	fmt.Println(t1, t2, t3)
+}
 
-	// 字符串拼接的三种方式
+// 字符串拼接的方式
+func joinStringTest() {
 	// 第一种：直接使用“+”号拼接
 	q1 := "hello"
 	q2 := ",world"
@@ -52,18 +71,49 @@ func stringTest() {
 	var result = fmt.Sprintf("%s%s", q1, q2)
 	fmt.Println(result)
 
-	// 字符串分割
+	// 第四种：使用strings.Join
+	url := "www.baidu.com"
+	strArray := strings.Split(url, ".")
+	fmt.Println(strings.Join(strArray, ","))
+}
+
+// 字符串分割
+func splitStringTest() {
 	url := "www.baidu.com"
 	strArray := strings.Split(url, ".")
 	fmt.Println(strArray)
-	// 字符串Join
-	fmt.Println(strings.Join(strArray, "."))
+}
+
+// 字符串前缀后缀判断
+func hasPrefixAndHasSuffixTest() {
+	url := "www.baidu.com"
 	// 前缀判断
 	fmt.Println(strings.HasPrefix(url, "www"))
 	// 后缀判断
 	fmt.Println(strings.HasSuffix(url, "com"))
+}
 
-	// 字符串遍历
+// 修改字符串中的字节：使用[]byte
+func updateStringByteTest() {
+	str := "hello world"
+	value := []byte(str) // 转换为[]byte
+	value[5] = ','       // 将空格替换为“,”
+	fmt.Printf("%s\n", str)
+	fmt.Printf("%s\n", value)
+}
+
+// 修改字符串中的字符：用[]rune
+func updateStringCharTest() {
+	str := "一梦三两年"
+	value := []rune(str) // 转换为[]rune
+	value[2] = '四'
+	value[3] = '五'
+	fmt.Println(str)
+	fmt.Println(string(value))
+}
+
+// 字符串遍历
+func foreachStringTest() {
 	p := "abc你好"
 	for i := 0; i < len(p); i++ {
 		fmt.Printf("%c \n", p[i])
@@ -73,31 +123,11 @@ func stringTest() {
 	for _, v := range p {
 		fmt.Printf("%c \n", v)
 	}
-
-	// 字符串修改
-	// 修改字符串中的字节：使用[]byte
-	ss := "hello world"
-	value := []byte(ss) // 转换为[]byte
-	value[5] = ','      // 将空格替换为“,”
-	fmt.Printf("%s\n", ss)
-	fmt.Printf("%s\n", value)
-
-	// 修改字符串中的字符：用[]rune
-	sss := "一梦三两年"
-	value2 := []rune(sss) // 转换为[]rune
-	value2[2] = '四'
-	value2[3] = '五'
-	fmt.Println(sss)
-	fmt.Println(string(value2))
 }
 
 // 内置strings包常见用法
 func stringsTest() {
 	s := "helloworld 世界你好"
-	fmt.Printf("string:%q\n", s)              // 原文格式输出
-	fmt.Printf("rune(char):%q\n", []rune(s))  // 输出[]rune切片
-	fmt.Printf("rune(hex):%x\n", []rune(s))   // 采用16进制数表示
-	fmt.Printf("bytes(hex):% x\n", []byte(s)) // 输出[]byte切片
 	// 判断一个字符串中是否包含某个子串
 	fmt.Printf("%t\n", strings.Contains(s, "world"))
 	// 检查字符串是不是以某个子串开始
@@ -130,9 +160,9 @@ func stringsTest() {
 
 // 练习题1：获取字符串中存在几个汉字
 func exercises1() {
-	name333 := "hello,勒布朗、科比、艾弗森"
+	name := "hello,勒布朗、科比、艾弗森"
 	var count int
-	for _, c := range name333 {
+	for _, c := range name {
 		if unicode.Is(unicode.Han, c) {
 			count++
 		}
--- src/dataType/complex/array.go
+++ src/dataType/complex/array.go
@@ -2,42 +2,56 @@ package main
 
 import "fmt"
 
-// 数组
-func arrayTest() {
-	// 数组创建，使用类型零值初始化
-	var a [3]int             // array of 3 integers
-	fmt.Println(a[0])        // print the first element
-	fmt.Println(a[len(a)-1]) // print the last element, a[2]
+func main() {
+	foreachArrayTest()
+}
 
-	// 数组创建使用数字字面值语法
-	var b [3]int = [3]int{1, 2, 3}
-	fmt.Println(b[0])
+// 数组创建,使用类型零值初始化
+func createArrayTest() {
+	var arr [3]int               // array of 3 integers
+	fmt.Println(arr[0])          // print the first element
+	fmt.Println(arr[len(arr)-1]) // print the last element, a[2]
+}
+
+// 数组创建,使用数字字面值语法
+func createArrayTest2() {
+	var arr [3]int = [3]int{1, 2, 3}
+	fmt.Println(arr[0])
 	// fmt.Println(a[10]) invalid array index 10 (out of bounds for 3-element array),数组越界
+}
 
-	//如果数组的长度位置出现的是“…”省略号，表示数组的长度是根据初始化值的个数来计算
-	c := [...]int{1, 2, 3}
-	fmt.Println(c[0])
+// 如果数组的长度位置出现的是“…”省略号，表示数组的长度是根据初始化值的个数来计算
+func createArrayTest3() {
+	arr := [...]int{1, 2, 3}
+	fmt.Println(arr[0])
+}
 
-	// 指定数组第1个元素值为100,第9个元素值为200,数组长度就是10
-	d := [...]int{1: 100, 9: 200}
-	fmt.Println(d)
+// 指定数组第1个元素值为100,第9个元素值为200,数组长度就是10
+func createArrayTest4() {
+	arr := [...]int{1: 100, 9: 200}
+	fmt.Println(arr)
+}
 
-	// 数组遍历
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("key:%d, value:%d\n", i, a[i])
+// 数组遍历
+func foreachArrayTest() {
+	var arr [3]int = [3]int{1, 2, 3}
+	for i := 0; i < len(arr); i++ {
+		fmt.Printf("index:%d, value:%d\n", i, arr[i])
 	}
 
 	// 使用range遍历数组
-	for i, v := range a {
-		fmt.Printf("key:%d, value:%d\n", i, v)
+	for i, v := range arr {
+		fmt.Printf("index:%d, value:%d\n", i, v)
 	}
 
 	// 使用range遍历数组:如果需要值但希望忽略索引，可以通过使用_ blank标识符替换索引来实现
-	for _, v := range a {
+	for _, v := range arr {
 		fmt.Printf("value:%d\n", v)
 	}
+}
 
-	// go同样支持多维数组
+// 多维数组
+func multidimensionalArrayTest() {
 	array := [3][4]int{
 		{0, 1, 2, 3},   /*  第一行索引为 0 */
 		{4, 5, 6, 7},   /*  第二行索引为 1 */
@@ -46,30 +60,39 @@ func arrayTest() {
 	for i, item := range array {
 		fmt.Printf("%d the element of is %v \n", i, item)
 	}
+}
 
-	// 数组比较：数组可以直接进行比较，当数组内的元素都一样的时候表示两个数组相等。
-	q := [...]int{11, 22, 33}
-	w := [...]int{11, 22, 33}
-	fmt.Println(q == w)
-	fmt.Println(q == b)
+// 数组比较：数组可以直接进行比较，当数组内的元素都一样的时候表示两个数组相等
+func compareArrayTest() {
+	arr1 := [...]int{11, 22, 33}
+	arr2 := [...]int{11, 22, 33}
+	arr3 := [...]int{1, 2, 3}
 
+	fmt.Println(arr1 == arr2)
+	fmt.Println(arr1 == arr3)
+}
+
+// 数组
+func arrayTest() {
 	// 数组是值类型：意味着当它被分配给一个新变量时，将把原始数组的副本分配给新变量。如果对新变量进行了更改不会在原始数组中反映
 	e := [...]int{11, 22, 33}
 	fmt.Println(e[0])
-	arrayTest2(e)
+	passByValue(e)
 	fmt.Println(e[0])
 
 	// 使用指针后函数内部对数组的更改将反应到原数组上
 	r := [...]int{11, 22, 33}
 	fmt.Println(r[0])
-	arrayTest3(&r)
+	passByReference(&r)
 	fmt.Println(r[0])
 }
 
-func arrayTest2(arr [3]int) {
+// 数组当参数传递,对数组进行了更改不会在原始数组中反映
+func passByValue(arr [3]int) {
 	arr[0] = 1
 }
 
-func arrayTest3(arr *[3]int) {
+// 使用指针后函数内部对数组的更改将反应到原数组上
+func passByReference(arr *[3]int) {
 	arr[0] = 100
 }
