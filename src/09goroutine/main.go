0797c232e86288cedb058bba7cc5fa284bd09012
WangPengLiang <15101587969@163.com>
2022-03-31 16:55:31 +0800

    add goroutine

---
+++ src/09goroutine/main.go
@@ -0,0 +1,25 @@
+// @title 《Go语言编程》-并发编程
+// @description
+// @author wangpengliang
+// @date 2022-03-31 16:16:58
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func hello(i int) {
+	defer wg.Done() // goroutine结束就登记-1
+	fmt.Println("hello", i)
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		wg.Add(1) // 启动一个goroutine就登记+1
+		go hello(i)
+	}
+	wg.Wait() // 等待所有登记的goroutine都结束
+}
