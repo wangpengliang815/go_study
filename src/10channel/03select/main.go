de3f1ca0eec86195b17b65d48dd8640cf33dd2cb
WangPengLiang <15101587969@163.com>
2022-04-02 11:14:25 +0800

    all goroutines are asleep - deadlock! confused!!!

--- src/10channel/03select/main.go
+++ src/10channel/03select/main.go
@@ -8,28 +8,34 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	var chs [2]chan int
-	chs[0] = make(chan int, 10)
-	chs[1] = make(chan int, 10)
+	var ch1 = make(chan int, 10)
+	// var ch2 = make(chan bool, 10)
 
-	go func() {
+	go func(chan int) {
 		defer wg.Done()
-		for v := range chs[0] {
+		for v := range ch1 {
 			fmt.Println(v)
 		}
-	}()
+	}(ch1)
 
-	go func() {
-		defer wg.Done()
-		for v := range chs[1] {
-			fmt.Println(v)
-		}
-	}()
+	// go func(chan bool) {
+	// 	defer wg.Done()
+	// 	for v := range ch2 {
+	// 		fmt.Println(v)
+	// 	}
+	// }(ch2)
 
-	for i := 0; i < len(chs); i++ {
-		chs[i] <- 100
+	for i := 0; i < 4; i++ {
 		wg.Add(1)
-		fmt.Printf("chs[%v]写入100完成 \n", i)
+		ch1 <- i
 	}
+	close(ch1)
+
+	// for i := 0; i < 3; i++ {
+	// 	wg.Add(1)
+	// 	ch2 <- false
+	// }
+	// close(ch2)
+
 	wg.Wait()
 }
