239f6dbcfe98d7aea5bbb4b27a9ad75551f3083e
WangPengLiang <15101587969@163.com>
2022-04-02 14:48:36 +0800

    update

--- src/10channel/03select/main.go
+++ src/10channel/03select/main.go
@@ -8,34 +8,12 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	var ch1 = make(chan int, 10)
-	// var ch2 = make(chan bool, 10)
-
-	go func(chan int) {
-		defer wg.Done()
-		for v := range ch1 {
-			fmt.Println(v)
+	ch := make(chan int, 1)
+	for i := 1; i <= 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
 		}
-	}(ch1)
-
-	// go func(chan bool) {
-	// 	defer wg.Done()
-	// 	for v := range ch2 {
-	// 		fmt.Println(v)
-	// 	}
-	// }(ch2)
-
-	for i := 0; i < 4; i++ {
-		wg.Add(1)
-		ch1 <- i
 	}
-	close(ch1)
-
-	// for i := 0; i < 3; i++ {
-	// 	wg.Add(1)
-	// 	ch2 <- false
-	// }
-	// close(ch2)
-
-	wg.Wait()
 }
